32092a8dc2094c50d7b85e82ae443b39e24327c0
geison.camara <geison.camara@digitaldesk.com.br>
2020-08-18 18:00:48 -0300

    git init

---
+++ PlaygroundPages/PlaygroundPages/Pages/Carousel/Step1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlaygroundPages.Pages.Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Step1 : ContentPage
+    {
+        public Step1()
+        {
+            InitializeComponent();
+        }
+    }
+}
